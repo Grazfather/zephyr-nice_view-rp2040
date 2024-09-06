32aad0973d6e4f4c831cb8c038defa8b72e75001
Grazfather <grazfather@gmail.com>
2024-09-05 23:03:40 -0400

    Get USB CDC and display working

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 
 #define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
@@ -6,6 +7,7 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
+#include <zephyr/drivers/display.h>
 
 #if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
 #define STRIP_NUM_PIXELS DT_PROP(DT_ALIAS(led_strip), chain_length)
@@ -25,6 +27,42 @@ static const struct led_rgb colors[] = {
 
 static struct led_rgb pixels[STRIP_NUM_PIXELS];
 static const struct device *const strip = DEVICE_DT_GET(DT_ALIAS(led_strip));
+
+static const struct device *const display = DEVICE_DT_GET(DT_ALIAS(nice_view));
+
+enum corner {
+	TOP_LEFT,
+	TOP_RIGHT,
+	BOTTOM_RIGHT,
+	BOTTOM_LEFT
+};
+
+typedef void (*fill_buffer)(enum corner corner, uint8_t grey, uint8_t *buf,
+			    size_t buf_size);
+
+static void fill_buffer_mono(enum corner corner, uint8_t grey,
+			     uint8_t black, uint8_t white,
+			     uint8_t *buf, size_t buf_size)
+{
+	uint16_t color;
+
+	switch (corner) {
+	case BOTTOM_LEFT:
+		color = (grey & 0x01u) ? white : black;
+		break;
+	default:
+		color = black;
+		break;
+	}
+
+	memset(buf, color, buf_size);
+}
+
+static inline void fill_buffer_mono01(enum corner corner, uint8_t grey,
+				      uint8_t *buf, size_t buf_size)
+{
+	fill_buffer_mono(corner, grey, 0x00u, 0xFFu, buf, buf_size);
+}
 
 static void led_thread_fn(void *a, void *b, void *c)
 {
@@ -50,6 +88,18 @@ struct k_thread led_thread;
 
 int main(void)
 {
+	size_t x;
+	size_t y;
+	size_t h_step;
+	size_t grey_count;
+	uint8_t bg_color;
+	uint8_t *buf;
+	int32_t grey_scale_sleep;
+	struct display_capabilities capabilities;
+	struct display_buffer_descriptor buf_desc;
+	size_t buf_size = 0;
+	fill_buffer fill_buffer_fnc = NULL;
+
 	if (device_is_ready(strip)) {
 		LOG_INF("Found LED device %s", strip->name);
 	} else {
@@ -64,8 +114,58 @@ int main(void)
 			NULL, NULL, NULL,
 			3, 0, K_NO_WAIT);
 
+	if (device_is_ready(display)) {
+		LOG_INF("Found display device %s", display->name);
+	} else {
+		LOG_ERR("display device %s is not ready", display->name);
+		return 0;
+	}
+
+	display_get_capabilities(display, &capabilities);
+
+	if (capabilities.screen_info & SCREEN_INFO_EPD) {
+		grey_scale_sleep = 10000;
+	} else {
+		grey_scale_sleep = 100;
+	}
+
+	buf_size = (capabilities.x_resolution * capabilities.y_resolution) / 8;
+
+	bg_color = 0xFFu;
+	fill_buffer_fnc = fill_buffer_mono01;
+
+	buf = malloc(buf_size);
+
+	if (buf == NULL) {
+		LOG_ERR("Could not allocate memory. Aborting app.");
+		return 0;
+	}
+
+	(void)memset(buf, bg_color, buf_size);
+
+	buf_desc.buf_size = buf_size;
+	buf_desc.height = 8;
+	buf_desc.pitch = capabilities.x_resolution;
+	buf_desc.width = capabilities.x_resolution;
+
+	fill_buffer_fnc(TOP_LEFT, 0, buf, buf_size);
+	x = 0;
+	y = 0;
+	display_write(display, x, y, &buf_desc, buf);
+
+	display_blanking_off(display);
+
+	grey_count = 0;
+	x = 0;
+	y = 0;
+
 	while (1) {
-		k_sleep(K_MSEC(20000));
+		fill_buffer_fnc(BOTTOM_LEFT, grey_count, buf, buf_size);
+		display_write(display, x, y, &buf_desc, buf);
+		++grey_count;
+		k_msleep(grey_scale_sleep);
+		buf_desc.height += 8;
+		buf_desc.height = buf_desc.height % 68;
 	}
 
 	return 0;
